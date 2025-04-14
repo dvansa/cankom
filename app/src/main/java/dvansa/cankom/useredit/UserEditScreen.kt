914b6efc8f7e6acf17983a138600ab7257b49aba
Daniel van Sabben <13849741+dvansa@users.noreply.github.com>
2025-04-14 21:02:37 +0200

    Temperature range sliders

--- app/src/main/java/dvansa/cankom/useredit/UserEditScreen.kt
+++ app/src/main/java/dvansa/cankom/useredit/UserEditScreen.kt
@@ -23,32 +23,61 @@
 */
 package dvansa.cankom.useredit
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import dvansa.cankom.R
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserEditScreen(
     onSaveUserParameters: () -> Unit,
     onBack: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Scaffold() { paddingValues ->
-        Column(modifier = modifier.padding(paddingValues)) {
-            Text(text = "User edit screen")
-            IconButton(onClick = {
-                onBack();
-            }) {
-                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+
+    Scaffold(modifier = modifier.fillMaxSize()) { paddingValues ->
+        Column(modifier = modifier.padding(paddingValues) /*horizontalAlignment = Alignment.CenterHorizontally*/) {
+            Box(contentAlignment = Alignment.TopStart) {
+                IconButton(onClick = {
+                    onBack();
+                }) {
+                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                }
             }
+            TemperatureSlider(value = 0 /*TODO pass state value*/, label = "Minimum Temperature", minValue = -20, maxValue = 20, onTemperatureChange={ temperature -> /*TODO update model println("On value change $temperature")*/;});
+            TemperatureSlider(value = 10 /*TODO pass state value*/, label = "Maximum Temperature", minValue = 0, maxValue = 40, onTemperatureChange={ temperature -> /*TODO update model*/});
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TemperatureSlider(
+    value: Int,
+    label: String,
+    minValue : Int = -10,
+    maxValue : Int = 10,
+    onTemperatureChange: (Int) -> Unit
+) {
+    Text(text = "$label - $value °C", modifier = Modifier.padding(dimensionResource(R.dimen.horizontal_margin)))
+    val barValue = (value - minValue).toFloat() / (maxValue - minValue).toFloat();
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth()) {
+        Slider(value= barValue, steps = maxValue - minValue + 1,onValueChange={value: Float -> onTemperatureChange( ((maxValue - minValue) * value).toInt());},modifier= Modifier.fillMaxWidth(0.7f));
     }
 }
